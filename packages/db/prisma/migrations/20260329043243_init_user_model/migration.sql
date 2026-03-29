ca324eb985a0ba47f0f00336075c9afca6a2a5bc
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-03-29 10:09:54 +0530

    chore: setup packages/db with prisma and user schema

---
+++ packages/db/prisma/migrations/20260329043243_init_user_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "capital" DOUBLE PRECISION NOT NULL DEFAULT 100000,
+    "dailyLossLimit" DOUBLE PRECISION NOT NULL DEFAULT 2000,
+    "maxTradesPerDay" INTEGER NOT NULL DEFAULT 6,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
